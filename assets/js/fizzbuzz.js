cef05313768410a1df234bc8daefa946de28eb6a
Piktoll <114651661+piktoll@users.noreply.github.com>
2022-11-24 21:52:50 +0100

    Update fizzbuzz.js

--- assets/js/fizzbuzz.js
+++ assets/js/fizzbuzz.js
@@ -1,12 +1,15 @@
-let echo = document.getElementById("echo-js");
+window.onload = function () {
+    let echo = document.getElementById("echo-js");
 
-for (i = 1; i < 100; i++) {
-    if (i % 3 == 0) {
-        echo.innerHTML += `Fizz`;
-    } else if (i % 5 == 0) {
-        echo.innerHTML += `Buzz`;
-    } else {
-        echo.innerHTML += `${i}`;
+    for (i = 1; i < 100; i++) {
+        if (i % 3 == 0) {
+            echo.innerHTML += `Fizz`;
+        } else if (i % 5 == 0) {
+            echo.innerHTML += `Buzz`;
+        } else {
+            echo.innerHTML += `${i}`;
+        }
+
+        echo.innerHTML += ` `;
     }
-    echo.innerHTML += ` `;
 }
