d0f928dc9b37eec1471bd9e01fe439f4bf6d37cf
Piktoll <114651661+piktoll@users.noreply.github.com>
2022-11-24 13:09:54 +0100

    Create fizzbuzz.php

---
+++ fizzbuzz.php
@@ -0,0 +1,28 @@
+<?php
+    $title = 'FizzBuzz';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
+    </head>
+    <body>
+            <p id="echo-php" class="result">
+                <?php
+                    for ($i = 1; $i < 100; $i++) {
+                        if ($i % 3 == 0) {
+                            echo 'Fizz';
+                        } else if ($i % 5 == 0) {
+                            echo 'Buzz';
+                        } else {
+                            echo $i;
+                        }
+                        echo ' ';
+                    }
+                ?>
+            </p>
+            <p id="echo-js" class="result"></p>
+        <script src="./assets/js/fizzbuzz.js" type="text/javascript"></script>
+    </body>
+</html>
