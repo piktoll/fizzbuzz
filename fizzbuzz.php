5638eddc08d843593529553b9b55dbec0d5bdb9e
Piktoll <114651661+piktoll@users.noreply.github.com>
2022-11-25 16:26:01 +0100

    Update fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,14 +1,30 @@
 <?php
-    $title = 'FizzBuzz';
+
+class Application {
+    public $title;
+    public $desc;
+
+    function __construct($title, $desc) {
+        $this->title = $title;
+        $this->desc = $desc;
+    }
+}
+
+$app = new Application('Fizzbuzz', 'A popular interview screening exercise.');
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php echo $title; ?></title>
+        <title><?php echo $app->title; ?></title>
         <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
         <script src="./assets/js/fizzbuzz.js" type="text/javascript"></script>
     </head>
     <body>
+        <header>
+            <h1><?php echo $app->title; ?></h1>
+            <h3><?php echo $app->desc; ?></h3>
+        </header>
+        <div class="result-container">
             <p id="echo-php" class="result">
                 <?php
                     for ($i = 1; $i < 100; $i++) {
@@ -24,5 +40,6 @@
                 ?>
             </p>
             <p id="echo-js" class="result"></p>
+        </div>
     </body>
 </html>
